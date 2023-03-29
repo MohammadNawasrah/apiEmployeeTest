c90c21d629552211ccd9b03b9003f782a934ff7d
Mohammad Al Nawasrah <nawasrahmohammad2000@gmail.com>
2023-03-29 20:10:28 +0300

    add employee design dialog

---
+++ lib/view/widget/HomePage/DialogAddEmployee.dart
@@ -0,0 +1,24 @@
+// ignore: duplicate_ignore
+// ignore: file_names
+// ignore_for_file: file_names
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class DialogAddEmployee extends StatelessWidget {
+  const DialogAddEmployee({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        TextField(
+          decoration: InputDecoration(label: Text("Name")),
+        ),
+        TextField(decoration: InputDecoration(label: Text("Age"))),
+        TextField(decoration: InputDecoration(label: Text("Email"))),
+        TextField(decoration: InputDecoration(label: Text("Location"))),
+        TextField(decoration: InputDecoration(label: Text("Department"))),
+      ],
+    );
+  }
+}
